cafddfdceec917aad13f7d786120cfc901a41c13
Jose Álvarez <josealvarezvivar@gmail.com>
2020-07-19 19:19:03 +0200

    adding functional cursor

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import const.initials as cinitials
 import const.files as cfiles
 import helpers.files as hfiles
 import helpers.jsonparser as jparser
+import const.colors as colors
 
 def chooseInitialPokemon():
 
@@ -39,23 +40,75 @@ def chooseInitialPokemon():
 
     return myInitial
 
+
+
 # Starting game
 
 # chosenPkmn = chooseInitialPokemon()
 # chosenPkmn.describePokemon()
 
+
 pygame.init()
 
-size = width, height = 320, 240
+size = width, height = 350, 240
 speed = [2,2]
-black = 0, 0, 0
+horizontalSpace = 70
+verticalSpace = 70
+cursorY = 0
+cursorX = 0
 
 screen = pygame.display.set_mode(size)
 
 while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
     
+    for event in pygame.event.get():
+        # Add listener to close the window
+        if event.type == pygame.QUIT: sys.exit()
 
-    screen.fill(black)
-    pygame.display.flip()
+        # Add listener to the cursor
+        if pygame.key.get_pressed()[pygame.K_RIGHT]:
+            cursorX += 70
+        if pygame.key.get_pressed()[pygame.K_LEFT]:
+            cursorX -= 70
+        if pygame.key.get_pressed()[pygame.K_UP]:
+            cursorY -= 70
+        if pygame.key.get_pressed()[pygame.K_DOWN]:
+            cursorY += 70
+
+    # Add some color and title
+    screen.fill(colors.WHITE)
+
+    # Get pokemons in a matrix
+    pkmnMatrix = []
+    pkmnRow = []
+    pkmnJSON = hfiles.readAllPkmn(cfiles.PKMN_JSON_PATH)
+    counter = 0
+
+    for currentPkmn in pkmnJSON:
+        
+        pkmnRow.append(currentPkmn)
+        counter += 1
+
+        if counter == 5:
+            pkmnMatrix.append(pkmnRow[:])
+            counter = 0
+            pkmnRow = []
+
+    # Add images
+    currentVertical = 0
+    for currentRow in pkmnMatrix:
+        currentHorizontal = 0
+        
+        for currentPkmn in currentRow:
+            imageToLoad = pygame.image.load(cfiles.PKMN_IMAGES_PATH + "/front-{}.png".format(currentPkmn["Nº"]))
+            screen.blit(imageToLoad, (currentHorizontal, currentVertical))
+            currentHorizontal += horizontalSpace
+        
+        currentVertical += verticalSpace
+
+    # Add cursor
+    pygame.draw.rect(screen, colors.BLACK, pygame.Rect(cursorX, cursorY, 60, 60), 4)
+
+
+
+    pygame.display.update()
