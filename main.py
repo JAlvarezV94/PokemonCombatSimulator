5c115b2c8b7681c423d1769e831b747fb6101d0c
Jose Álvarez <josealvarezvivar@gmail.com>
2020-07-19 20:20:36 +0200

    adding a configuration file and showing only three rows at the same time

--- main.py
+++ main.py
@@ -8,55 +8,20 @@ import const.files as cfiles
 import helpers.files as hfiles
 import helpers.jsonparser as jparser
 import const.colors as colors
+import configuration as config
 
-def chooseInitialPokemon():
-
-    # Get initial's info
-
-    initial1 = jparser.parsePokemon(hfiles.readPkmnByIndex(cfiles.PKMN_JSON_PATH, cinitials.BULBASAUR))
-    initial2 = jparser.parsePokemon(hfiles.readPkmnByIndex(cfiles.PKMN_JSON_PATH, cinitials.CHARMANDER))
-    initial3 = jparser.parsePokemon(hfiles.readPkmnByIndex(cfiles.PKMN_JSON_PATH, cinitials.SQUIRTLE))
-
-    initials = [initial1, initial2, initial3]
-
-    # Choosing initial
-
-    myInitial = None
-    
-    while myInitial == None:
-        counter = 1
-        print("What pokemon do you choose?")
-
-        for currentPkmn in initials:
-            print(str(counter) + " - {} the {} type pokemon.".format(initials[counter - 1].name, initials[counter - 1].type))
-            counter += 1
-
-        response = input()
-        
-        if response == str(1) or response == str(2) or response == str(3):
-            myInitial = initials[int(response) - 1]
-        else:
-            os.system("clear")
-
-    return myInitial
-
-
+# Initializing parameters
+size = width, height = config.SCREEN_WIDTH, config.SCREEN_HEIGHT
+speed = [2,2]
+horizontalSpace = config.HORIZONTAL_SPACE
+verticalSpace = config.VERTICAL_SPACE
+cursorY = config.CUSROR_INITIAL_POSITION_Y
+cursorX = config.CUSROR_INITIAL_POSITION_X
+firstRow = 0
 
 # Starting game
-
-# chosenPkmn = chooseInitialPokemon()
-# chosenPkmn.describePokemon()
-
-
 pygame.init()
-
-size = width, height = 350, 240
-speed = [2,2]
-horizontalSpace = 70
-verticalSpace = 70
-cursorY = 0
-cursorX = 0
-
+pygame.display.set_caption('Pkmn Combat Simulator')
 screen = pygame.display.set_mode(size)
 
 while 1:
@@ -65,15 +30,15 @@ while 1:
         # Add listener to close the window
         if event.type == pygame.QUIT: sys.exit()
 
-        # Add listener to the cursor
+        # Add listener to move the cursor
         if pygame.key.get_pressed()[pygame.K_RIGHT]:
-            cursorX += 70
+            cursorX += config.HORIZONTAL_SPACE
         if pygame.key.get_pressed()[pygame.K_LEFT]:
-            cursorX -= 70
+            cursorX -= config.HORIZONTAL_SPACE
         if pygame.key.get_pressed()[pygame.K_UP]:
-            cursorY -= 70
+            cursorY -= config.VERTICAL_SPACE
         if pygame.key.get_pressed()[pygame.K_DOWN]:
-            cursorY += 70
+            cursorY += config.VERTICAL_SPACE
 
     # Add some color and title
     screen.fill(colors.WHITE)
@@ -89,25 +54,25 @@ while 1:
         pkmnRow.append(currentPkmn)
         counter += 1
 
-        if counter == 5:
+        if counter == config.MAX_COLUMNS:
             pkmnMatrix.append(pkmnRow[:])
             counter = 0
             pkmnRow = []
 
-    # Add images
-    currentVertical = 0
-    for currentRow in pkmnMatrix:
-        currentHorizontal = 0
-        
+    # Display the matrix
+    currentVertical = config.INIT_VERTICAL
+    maxRows = firstRow + config.MAX_ROWS
+    for index in range(firstRow, maxRows):
+        currentHorizontal = config.INIT_HORIZONTAL
+        currentRow = pkmnMatrix[index]
         for currentPkmn in currentRow:
             imageToLoad = pygame.image.load(cfiles.PKMN_IMAGES_PATH + "/front-{}.png".format(currentPkmn["Nº"]))
             screen.blit(imageToLoad, (currentHorizontal, currentVertical))
             currentHorizontal += horizontalSpace
-        
         currentVertical += verticalSpace
 
     # Add cursor
-    pygame.draw.rect(screen, colors.BLACK, pygame.Rect(cursorX, cursorY, 60, 60), 4)
+    pygame.draw.rect(screen, colors.BLACK, pygame.Rect(cursorX, cursorY, config.CURSOR_WIDTH, config.CURSOR_HEIGHT), 4)
 
 
 
